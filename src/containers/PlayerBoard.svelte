518959af862d6676e9eb98814ee0750b419b768e
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2020-03-14 12:10:37 +0100

    add PlayerSelectorLayout

--- src/containers/PlayerBoard.svelte
+++ src/containers/PlayerBoard.svelte
@@ -2,10 +2,13 @@
   import Layout from "../layouts/PlayerBoardLayout.svelte";
   import PlayerTilesLayout from "../layouts/PlayerBoard/PlayerTilesLayout.svelte";
   import PlayerBuildingLayout from "../layouts/PlayerBoard/PlayerBuildingsLayout.svelte";
+  import PlayerSelectorLayout from "../layouts/PlayerBoard/PlayerSelectorLayout.svelte";
   import PlayerProfile from "../components/PlayerProfile.svelte";
 </script>
 
-<div class="h-full bg-no-repeat bg-cover" style="background-image: url(./img/board.jpg)">
+<div
+  class="h-full bg-no-repeat bg-cover"
+  style="background-image: url(./img/board.jpg)">
   <Layout>
     <div class="h-full" slot="playerProfile">
       <PlayerProfile />
@@ -15,6 +18,9 @@
     </div>
     <div class="h-full" slot="playerBuildings">
       <PlayerBuildingLayout />
+    </div>
+    <div class="h-full" slot="playerSelector">
+      <PlayerSelectorLayout players={[...Array(4).keys()]} />
     </div>
   </Layout>
 </div>
