c296c4217d59657aa843111f42533ce7e26099d2
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2020-03-14 11:49:30 +0100

    update PlayerBoard container

--- src/containers/PlayerBoard.svelte
+++ src/containers/PlayerBoard.svelte
@@ -1,10 +1,20 @@
 <script>
   import Layout from "../layouts/PlayerBoardLayout.svelte";
+  import PlayerTilesLayout from "../layouts/PlayerBoard/PlayerTilesLayout.svelte";
+  import PlayerBuildingLayout from "../layouts/PlayerBoard/PlayerBuildingsLayout.svelte";
   import PlayerProfile from "../components/PlayerProfile.svelte";
 </script>
 
-<Layout>
-  <div slot="playerProfile">
-    <PlayerProfile />
-  </div>
-</Layout>
+<div class="h-full bg-no-repeat bg-cover" style="background-image: url(./img/board.jpg)">
+  <Layout>
+    <div class="h-full" slot="playerProfile">
+      <PlayerProfile />
+    </div>
+    <div class="h-full" slot="playerTiles">
+      <PlayerTilesLayout />
+    </div>
+    <div class="h-full" slot="playerBuildings">
+      <PlayerBuildingLayout />
+    </div>
+  </Layout>
+</div>
