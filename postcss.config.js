280df2d75dbbc9c303b5e617036f88a6c709e04d
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2020-03-14 12:37:34 +0100

    whitelist grid utility classes to enable dynamic class names

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,11 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
         './src/**/*.svelte'
     ],
 
-    whitelistPatterns: [/svelte-/],
+    whitelistPatterns: [
+        /svelte-/,
+        /grid-cols-[1-9]+/,
+        /grid-rows-[1-9]+/
+    ],
 
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
