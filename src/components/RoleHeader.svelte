72b51f0593d2ae135423ef182ef192b27f43f37b
Dániel Marosváry <marosvaryd@gmail.com>
2020-04-23 18:35:52 +0200

    rol

--- src/components/RoleHeader.svelte
+++ src/components/RoleHeader.svelte
@@ -3,15 +3,27 @@
 </script>
 
 <div class="flex flex-row justify-center items-center bg-primary">
-  <div class="flex-initial w-6">
-    <img src={`./img/roles/${roleName}.svg`} alt="*" />
-  </div>
+  {#if roleName == 'Mayor' || roleName == 'Settler'}
+    <div class="flex-initial w-6 p-1">
+      <img src={`./img/roles/${roleName}Icon.svg`} alt="*" />
+    </div>
+  {:else}
+    <div class="flex-initial w-6">
+      <img src={`./img/roles/${roleName}Icon.svg`} alt="*" />
+    </div>
+  {/if}
   <div
     class="flex-initial text-center text-text font-minion text-xl tracking-wide
-    font-semibold uppercase mx-3">
+    font-semibold uppercase mx-5">
     {roleName}
   </div>
-  <div class="flex-initial w-6">
-    <img src={`./img/roles/${roleName}.svg`} alt="*" />
-  </div>
+  {#if roleName == 'Mayor' || roleName == 'Settler' }
+    <div class="flex-initial w-6 p-1">
+      <img src={`./img/roles/${roleName}Icon.svg`} alt="*" />
+    </div>
+  {:else}
+    <div class="flex-initial w-6">
+      <img src={`./img/roles/${roleName}Icon.svg`} alt="*" />
+    </div>
+  {/if}
 </div>
