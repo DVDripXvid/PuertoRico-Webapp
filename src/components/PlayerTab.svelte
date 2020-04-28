ae054f1b4d41e90716cef6b0902f33193456967a
Rottenhoffer Olivér <orottenhoffer@geomant.com>
2020-04-28 20:54:11 +0200

    pulse when current user selected another player & fix interactable

--- src/components/PlayerTab.svelte
+++ src/components/PlayerTab.svelte
@@ -4,7 +4,31 @@
   export let username;
   export let imageUrl;
   export let isCurrent = false;
+  export let isSignedIn = false;
+  export let isSelected = false;
+
+  $: pulse = isSignedIn && isCurrent && !isSelected ;
 </script>
+
+<style>
+  @keyframes downScale {
+    from {
+      transform: scale(1);
+      -webkit-transform: scale(1);
+    }
+    to {
+      transform: scale(0.75);
+      -webkit-transform: scale(0.75);
+    }
+  }
+
+  .pulse {
+    animation-duration: 0.75s;
+    animation-name: downScale;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+</style>
 
 <div class="flex flex-row justify-center items-center" on:click>
   <div class="flex-initial w-1/4">
@@ -13,8 +37,9 @@
     {/if}
   </div>
   <div
-    class="cursor-pointer flex-initial w-full transform transition-all duration-500
-    hover:scale-75">
+    class:pulse
+    class="cursor-pointer flex-initial w-full transform transition-all
+    duration-500 hover:scale-75">
     <PlayerProfile {username} {imageUrl} />
   </div>
 </div>
