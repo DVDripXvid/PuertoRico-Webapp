a44e29c947ab4dc1114f54ac56ff16634b75991f
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2021-01-17 08:47:00 +0100

    update config

--- src/config.js
+++ src/config.js
@@ -3,14 +3,11 @@ export function getConfig() {
 
     const hostConfigs = {
         'localhost': {
-            hubUrl: 'https://puertorico.azurewebsites.net/lobby'
+            hubUrl: 'https://puerto-rico-api.herokuapp.com/lobby'
             // hubUrl: 'http://localhost:5000/lobby'
         },
-        'storagepuertodev.z16.web.core.windows.net': {
-            hubUrl: 'https://puertorico.azurewebsites.net/lobby'
-        },
-        'storagepuertodevstage.z16.web.core.windows.net': {
-            hubUrl: 'https://puertorico-devstage.azurewebsites.net/lobby'
+        'puertorico.netlify.app': {
+            hubUrl: 'https://puerto-rico-api.herokuapp.com/lobby'
         }
     }
 
